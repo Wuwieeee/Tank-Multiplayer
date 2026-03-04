edc58b07b4abb424a6b2834f2ece81cec1451cd1
Punchuda Kulsrichanarat <punchuda.kuls@bumail.net>
2026-03-04 22:20:13 +0700

    Set up

---
+++ Assets/Script/Network/MainMenu.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField joinCodeField;
+
+    private void Start()
+    {
+        if (ClientSingleton.Instance == null)
+        {
+            return;
+        }
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    public async void StartHost()
+    {
+        await HostSingleton.Instance.GameManager.StartHostAsync();
+    }
+
+    public async void StartClient()
+    {
+        await ClientSingleton.Instance.GameManager.StartClientAsync(joinCodeField.text);
+    }
+}
+
